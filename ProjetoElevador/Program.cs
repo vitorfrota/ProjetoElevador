bbe2d9160ffed60a947f0ae325afc2274643e5cc
Vitor <vitorfrota@live.com>
2021-12-21 18:31:42 -0400

    Adicionar arquivos de projeto.

---
+++ ProjetoElevador/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
